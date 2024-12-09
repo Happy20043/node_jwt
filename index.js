2dc70bd93574c8b282df463a1be5804bc85bfb04
Happy20043 <125646658+Happy20043@users.noreply.github.com>
2024-12-09 12:09:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ import bodyParser from "body-parser";
 import express from "express";
 import DbConnect from "./middleware/dbConnect.js";
 import serverRoutes from "./routes/serverRoutes.js";
-import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import path from "path";
+import fs from "fs";
 
+dotenv.config();
 
+DbConnect();
 const app = express();
 app.use(express.json());
-app.use(DbConnect);
+// app.use();
 app.use(bodyParser.json());
 app.use("/", (req, res, next) => {
   next();
-});
+}); 
 
 app.use("/", serverRoutes);
 
-app.listen(6200);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
