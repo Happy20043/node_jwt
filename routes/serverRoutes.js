3cdb69f21d45bbfc10c68c0249908a7677d788f3
Happy20043 <125646658+Happy20043@users.noreply.github.com>
2024-12-09 12:06:38 +0530

    Update serverRoutes.js

--- routes/serverRoutes.js
+++ routes/serverRoutes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 import userRoutes from "./user.js";
 import authRoutes from "./auth.js";
+import blogRoutes from "./blog.js";
 
 router.use("/user", userRoutes);
 router.use('/auth', authRoutes);
+router.use('/blog', blogRoutes);
 
 export default router;
