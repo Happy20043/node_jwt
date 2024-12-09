1b8953d8cf4c60e70509b740304aa0f7dea60f1d
Happy20043 <125646658+Happy20043@users.noreply.github.com>
2024-12-09 12:03:41 +0530

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -1,17 +1,32 @@
 import User from "../model/user.js";
 
-export const getUser = async (query) => {
-  return await User.find(query);
+export const getUser = async (req, res) => {
+  try {
+    const userData = await User.find(req.query);
+    res.send(userData);
+  } catch (error) {
+    console.log(error);
+    res.json(error.message);
+  }
 };
 
-export const createUser = async (user) => {
-  return await User.create(user);
-};
+export const updateUser = async (req, res) => {
+  console.log(req.body);
+  try {
+    const userData = await User.findByIdAndUpdate(req.params.id, req.body);
+    res.send(userData);
+  } catch (error) {
+    console.log(error);
+    res.json(error.message);
+  }
+}
 
-export const updateUser = async (id, user) => {
-  return await User.findByIdAndUpdate(id, user, { new: true });
-};
-
-export const deleteUser = async (id, user) => {
-  return await User.findByIdAndDelete(id, user);
-};
+export const deleteUser = async (req, res) => {
+  try {
+    const userData = await User.findByIdAndDelete(req.params.id, req.body);
+    res.send({ userData, message: "delete successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json(error.message);
+  }
+}
